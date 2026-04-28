63f7f6645f34abcdf8f40063ead7b8ae9af2d7b6
Dastan <kanatdastan56@gmail.com>
2026-04-28 23:20:49 +0500

    add funtion to story wiki

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -8,24 +8,28 @@ const STORIES = [
     name: 'Геннадий Головкин',
     role: 'многократный чемпион мира',
     img: 'https://vlast.kz/media/pages/cb/1497851135t8dq5_1000x768.jpg',
+    href: 'https://ru.wikipedia.org/wiki/%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BA%D0%B8%D0%BD,_%D0%93%D0%B5%D0%BD%D0%BD%D0%B0%D0%B4%D0%B8%D0%B9_%D0%93%D0%B5%D0%BD%D0%BD%D0%B0%D0%B4%D1%8C%D0%B5%D0%B2%D0%B8%D1%87',
   },
   {
     id: 2,
     name: 'Димаш Кудайберген',
     role: 'мировой вокалист',
     img: 'https://media.cmn.kz/cmn-photobank/media/2025-12-24/5f9f2323-ce57-406c-87f9-4b580e2c6ee3.jpeg ',
+    href: 'https://ru.wikipedia.org/wiki/%D0%9A%D1%83%D0%B4%D0%B0%D0%B9%D0%B1%D0%B5%D1%80%D0%B3%D0%B5%D0%BD,_%D0%94%D0%B8%D0%BC%D0%B0%D1%88',
   },
   {
     id: 3,
     name: 'Кайрат Нуртас',
     role: 'эстрадный певец',
     img: 'https://avatars.mds.yandex.net/i?id=649a2011d721e6739429de2e7aae7a843b834d9a-4423990-images-thumbs&n=13',
+    href: 'https://ru.wikipedia.org/wiki/%D0%9D%D1%83%D1%80%D1%82%D0%B0%D1%81,_%D0%9A%D0%B0%D0%B9%D1%80%D0%B0%D1%82',
   },
   {
     id: 4,
     name: 'Нурлан Сабуров',
     role: 'Комик и юморист',
     img: 'https://img.championat.com/s/732x488/news/big/q/h/blagodaren-strane-nurlan-saburov-vpervye-prokommentiroval-zapret-na-vezd-v-rossiyu_177204496055914962.jpg',
+    href: 'https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%B1%D1%83%D1%80%D0%BE%D0%B2,_%D0%9D%D1%83%D1%80%D0%BB%D0%B0%D0%BD_%D0%90%D0%BB%D0%B8%D0%B1%D0%B5%D0%BA%D0%BE%D0%B2%D0%B8%D1%87',
   },
 ]
 
@@ -74,7 +78,7 @@ export default function Stories() {
               <div className={styles.body}>
                 <h3>{story.name}</h3>
                 <p>{story.role}</p>
-                <a href="#" className={`${s.btn} ${s.btnGold} ${styles.btn}`}>Подробнее</a>
+                <a href={story.href} target="_blank" className={`${s.btn} ${s.btnGold} ${styles.btn}`}>Подробнее</a>
               </div>
             </article>
           ))}
